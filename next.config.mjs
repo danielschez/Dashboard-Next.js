3c7c356e99bab394d072eb1a7dccf5ab2249c14d
Daniel Hernandez <daniel_hernandezs@outlook.es>
2024-09-09 14:01:16 -0600

    package fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
- 
+
 const nextConfig = {
-    experimental: {
-      ppr: 'incremental',
-    },
-  };
-   
-  export default nextConfig;
+  experimental: {
+    appDir: true, // Usualmente esta opción es válida para habilitar el uso de la carpeta 'app'
+  },
+};
+
+export default nextConfig;
