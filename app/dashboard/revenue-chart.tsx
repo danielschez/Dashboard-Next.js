4b4daaf069a1952399c43a9269fc16ed721f8ac7
Daniel Hernandez <daniel_hernandezs@outlook.es>
2024-09-09 14:56:41 -0600

    revenue-chart fix

--- app/dashboard/revenue-chart.tsx
+++ app/dashboard/revenue-chart.tsx
@@ -1,13 +1,19 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Image from 'next/image';
+import { generateYAxis } from '@/app/lib/utils';
+import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices } from '@/app/lib/data';
+import { fetchRevenue } from '@/app/lib/data';
  
-export default async function LatestInvoices() { // Remove props
-  const latestInvoices = await fetchLatestInvoices();
+// ...
  
-  return (
-    // ...
-  );
+export default async function RevenueChart() { // Make component async, remove the props
+  const revenue = await fetchRevenue(); // Fetch data inside the component
+ 
+  const chartHeight = 350;
+  const { yAxisLabels, topLabel } = generateYAxis(revenue);
+ 
+  if (!revenue || revenue.length === 0) {
+    return <p className="mt-4 text-gray-400">No data available.</p>;
+  }
+
 }
+ 
